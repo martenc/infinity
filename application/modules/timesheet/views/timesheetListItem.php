0819c8f656ff54fad352deec683d8603e3b31e2e
vijay04 <vijay.mayekar04@gmail.com>
2013-03-05 18:36:49 +0530

    add some formatting

--- application/modules/timesheet/views/timesheetListItem.php
+++ application/modules/timesheet/views/timesheetListItem.php
@@ -1,5 +1,5 @@
-<div class="item-wrapper">
-    <div class="description-wrapper"><?php print $item->description; ?></div>
-    <div class="project-wrapper"><?php print $item->projectName; ?></div>
-    <div class="total-time-wrapper"><?php print $item->total; ?></div>
+<div class="item-wrapper row-fluid">
+    <div class="description-wrapper span8"><?php print $item->description; ?></div>
+    <div class="project-wrapper span2"><?php print $item->projectName; ?></div>
+    <div class="total-time-wrapper span2"><?php print $item->total; ?></div>
 </div>
